749f610268fa4ef6663961f5d468d875bc184625
Ethan Escalante <ethan65g@gmail.com>
2024-01-31 20:30:31 -0800

    1/31 8:29PM changes (should be working, but test more)

---
+++ cmake-build-debug/CMakeFiles/cs315_project1_phase1.dir/DependInfo.cmake
@@ -0,0 +1,21 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "C:/Users/Ethan/CLionProjects/ethanevertescalante/Token.cpp" "CMakeFiles/cs315_project1_phase1.dir/Token.cpp.obj" "gcc" "CMakeFiles/cs315_project1_phase1.dir/Token.cpp.obj.d"
+  "C:/Users/Ethan/CLionProjects/ethanevertescalante/Tokenizer.cpp" "CMakeFiles/cs315_project1_phase1.dir/Tokenizer.cpp.obj" "gcc" "CMakeFiles/cs315_project1_phase1.dir/Tokenizer.cpp.obj.d"
+  "C:/Users/Ethan/CLionProjects/ethanevertescalante/main.cpp" "CMakeFiles/cs315_project1_phase1.dir/main.cpp.obj" "gcc" "CMakeFiles/cs315_project1_phase1.dir/main.cpp.obj.d"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ cmake-build-debug/CMakeFiles/cs315_project1_phase1.dir/cmake_clean.cmake
@@ -0,0 +1,17 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/cs315_project1_phase1.dir/Token.cpp.obj"
+  "CMakeFiles/cs315_project1_phase1.dir/Token.cpp.obj.d"
+  "CMakeFiles/cs315_project1_phase1.dir/Tokenizer.cpp.obj"
+  "CMakeFiles/cs315_project1_phase1.dir/Tokenizer.cpp.obj.d"
+  "CMakeFiles/cs315_project1_phase1.dir/main.cpp.obj"
+  "CMakeFiles/cs315_project1_phase1.dir/main.cpp.obj.d"
+  "cs315_project1_phase1.exe"
+  "cs315_project1_phase1.exe.manifest"
+  "cs315_project1_phase1.pdb"
+  "libcs315_project1_phase1.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/cs315_project1_phase1.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
